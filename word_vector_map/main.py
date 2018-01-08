a33daed2a8beeea01f1d0f8f67f3020ef8ef2d5d
Will LeVine <levinwil2@gmail.com>
2018-01-08 09:12:16 -0600

    added word vec map unit tests

--- word_vector_map/main.py
+++ word_vector_map/main.py
@@ -16,20 +16,20 @@ def __generate_word_num_tuples():
     vocab_tuples = [(vocab_set[i], i * 1.0 / num_vocab) for i in range(num_vocab)]
     return vocab_tuples
 
-def __generate_word_to_num_map():
+def generate_word_to_num_map():
     return dict(__generate_word_num_tuples())
 
-def __generate_num_to_word_map():
+def generate_num_to_word_map():
     reverse_map = __generate_word_num_tuples()
     return dict((v, k) for k, v in reverse_map)
 
 
 def sentence_to_vec(sentence):
     word_array = sentence.split(' ')
-    vocab_map = __generate_word_to_num_map()
+    vocab_map = generate_word_to_num_map()
     return map(lambda x: vocab_map.get(x), word_array)
 
 def vec_to_sentence(v):
-    vocab_map = __generate_num_to_word_map()
+    vocab_map = generate_num_to_word_map()
     word_array = map(lambda x: vocab_map.get(x), v)
-    return word_array.join(" ")
+    return " ".join(word_array)
---
+++ word_vector_map/test.py
@@ -0,0 +1,33 @@
+import unittest
+from main import *
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_word_to_num_map(self):
+        actual = {0.0: 'and', 0.5: 'one', 0.7142857142857143: 'the', 0.7857142857142857: 'more', 0.14285714285714285: 'final', 0.07142857142857142: 'word2vec', 0.5714285714285714: 'second', 0.21428571428571427: 'for', 0.42857142857142855: 'is', 0.6428571428571429: 'another', 0.9285714285714286: 'first', 0.2857142857142857: 'sentence', 0.8571428571428571: 'yet', 0.35714285714285715: 'this'}
+        self.assertEqual(generate_num_to_word_map(), actual)
+
+    def test_num_to_word_map(self):
+        actual = {'and': 0.0, 'word2vec': 0.07142857142857142, 'for': 0.21428571428571427, 'sentence': 0.2857142857142857, 'this': 0.35714285714285715, 'is': 0.42857142857142855, 'one': 0.5, 'second': 0.5714285714285714, 'another': 0.6428571428571429, 'the': 0.7142857142857143, 'first': 0.9285714285714286, 'yet': 0.8571428571428571, 'final': 0.14285714285714285, 'more': 0.7857142857142857}
+        self.assertEqual(generate_word_to_num_map(), actual)
+
+    def test_sentence_to_vec(self):
+        vector = [0.35714285714285715, 0.42857142857142855, 0.6428571428571429, 0.2857142857142857]
+        sentence = "this is another sentence"
+        self.assertEqual(sentence_to_vec(sentence), vector)
+
+    def test_vec_to_sentence(self):
+        vector = [0.35714285714285715, 0.42857142857142855, 0.6428571428571429, 0.2857142857142857]
+        sentence = "this is another sentence"
+        self.assertEqual(vec_to_sentence(vector), sentence)
+
+    def test_two_way_conversion_vec(self):
+        vector = [0.35714285714285715, 0.42857142857142855, 0.6428571428571429, 0.2857142857142857]
+        self.assertEqual(sentence_to_vec(vec_to_sentence(vector)), vector)
+
+    def test_two_way_conversion_sentence(self):
+        sentence = "this is another sentence"
+        self.assertEqual(vec_to_sentence(sentence_to_vec(sentence)), sentence)
+
+if __name__ == '__main__':
+    unittest.main()
